bbcaac601faaea0278c7c79fec19a54152bd17a2
Artemiy <renevartemiy@yandex.ru>
2024-11-17 20:41:29 +0300

    make better repo

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.api.routes import ROUTES
-from app.admin.views import get_admin
+from app.views import get_admin
 def setup_routers(app: FastAPI) -> None:
 	for prefix, router in ROUTES.items():
 		app.include_router(router, prefix=prefix)
